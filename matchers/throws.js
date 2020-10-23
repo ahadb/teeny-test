13242b5068bd5a650a42c1b62af7cf6aa8270b5f
Ahad Bokhari <ahadbokhari@gmail.com>
2020-10-23 12:27:15 -0400

    clean up

--- matchers/throws.js
+++ matchers/throws.js
@@ -1,17 +1,6 @@
-
-// module.exports = (fn, errMsg = '') => {
-//   const didNotThrow = new Error('The function did not throw an error');
-//   try {
-//     fn();
-//     throw didNotThrow;
-//   } catch (err) {
-//     if (err === didNotThrow) throw didNotThrow;
-//     if (!errMsg || err.message === errMsg) return true;
-//
-//     throw new Error('The error message is different from the expected one.' +
-//       `\n\nFound: ${err.message}\nWanted: ${errMsg}`);
-//   }
-// };
+/**
+ * tests if the target throws, can be faster without try / catch
+ */
 
 module.exports = (fn) => {
   try {
